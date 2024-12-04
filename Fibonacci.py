bf995ca964a2f9532712e22ed591edde6c0b3ef9
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-12-04 21:21:32 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci_series(n):
+    num1,num2=0,1
+    fibonacci_sequence = []
+    for _ in range(n):
+        num1,num2 = num2,num1+num2
+        fibonacci_sequence.append(num1)
+    return fibonacci_sequence
+nth=int(input("Enter a number 'n' for displaying fibonacci series upto 'n':"))
+result=fibonacci_series(nth)
+print(f"The Fibonacci Sequence: {result}")
